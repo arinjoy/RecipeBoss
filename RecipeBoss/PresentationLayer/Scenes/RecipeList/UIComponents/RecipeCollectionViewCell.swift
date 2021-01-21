0a4288681aef9bda915ddac459bf39442f7018aa
Arinjoy Biswas <arinjoy.biswas@hotmail.com>
2021-01-21 22:39:54 +1100

    moved files around new project structure

---
+++ RecipeBoss/PresentationLayer/Scenes/RecipeList/UIComponents/RecipeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCollectionViewCell.swift
+//  RecipeBoss
+//
+//  Created by Arinjoy Biswas on 21/1/21.
+//
+
+import UIKit
+
+final class RecipeCollectionViewCell: UICollectionViewCell, NibProvidable, ReusableView {
+
+    // MARK: - Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ RecipeBoss/PresentationLayer/Scenes/RecipeListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeListViewController.swift
+//  RecipeBoss
+//
+//  Created by Arinjoy Biswas on 21/1/21.
+//
+
+import UIKit
+
+final class RecipeListViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    @IBOutlet private var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+

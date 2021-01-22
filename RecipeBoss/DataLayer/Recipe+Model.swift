7452a6e12839fcdc0d44022d65bd09acb74ac7c8
Arinjoy Biswas <arinjoy.biswas@hotmail.com>
2021-01-22 22:31:53 +1100

    added data layer JSON mapping for recipes

---
+++ RecipeBoss/DataLayer/Recipe+Model.swift
@@ -0,0 +1,56 @@
+//
+//  Recipe+Model.swift
+//  RecipeBoss
+//
+//  Created by Arinjoy Biswas on 22/1/21.
+//
+
+import Foundation
+
+struct Recipe: Decodable {
+    
+    let title: String
+    let description: String
+    let thumbnailPath: String
+    let thumbnailAltText: String
+    let details: RecipeDetails
+    let ingredients: [Ingredient]
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "dynamicTitle"
+        case description = "dynamicDescription"
+        case thumbnailPath = "dynamicThumbnail"
+        case thumbnailAltText = "dynamicThumbnailAlt"
+        case details = "recipeDetails"
+        case ingredients = "ingredients"
+    }
+}
+
+struct RecipeDetails: Decodable {
+    
+    let amountLabel: String
+    let amount: Int
+    
+    let preparationTimeLabel: String
+    let preparationTime: Int
+    let preparationTimeText: String
+    
+    let cookingTimeLabel: String
+    let cookingTime: Int
+    let cookingTimeText: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case amountLabel = "amountLabel"
+        case amount = "amountNumber"
+        case preparationTimeLabel = "prepLabel"
+        case preparationTime = "prepTimeAsMinutes"
+        case preparationTimeText = "prepTime"
+        case cookingTimeLabel = "cookingTimeLabel"
+        case cookingTime = "cookTimeAsMinutes"
+        case cookingTimeText = "cookTime"
+    }
+}
+
+struct Ingredient: Decodable {
+    let item: String
+}
